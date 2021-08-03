f24db8463d2228c760d0c86bbb293c8d65c5df85
Daan Asscheman <dasscheman@gmail.com>
2021-08-03 16:22:51 +0200

    Type in migration

--- migrations/2021_07_30_093500_create_binshops_fields_table.php
+++ migrations/2021_07_30_093500_create_binshops_fields_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBinshopFieldsTable extends Migration
+class CreateBinshopsFieldsTable extends Migration
 {
     /**
      * Run the migrations.
--- migrations/2021_07_30_093700_create_binshops_fields_categories_table.php
+++ migrations/2021_07_30_093700_create_binshops_fields_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBinshopFieldsCategoriesTable extends Migration
+class CreateBinshopsFieldsCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
