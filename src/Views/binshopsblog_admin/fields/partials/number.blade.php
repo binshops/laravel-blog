e326de04c2230f0167c83453eaff90fd12d5daec
Daan Asscheman <dasscheman@gmail.com>
2021-08-04 16:17:49 +0200

    Allow decimals.

--- src/Views/binshopsblog_admin/fields/partials/number.blade.php
+++ src/Views/binshopsblog_admin/fields/partials/number.blade.php
@@ -1,6 +1,7 @@
 <div class="form-group">
     <label for="{{$field->name}}">{{$field->label}}</label>
     <input type="number"
+           step="any"
            class="form-control field_category {{$classes}}"
            id="{{$field->name}}"
            name="{{$field->name}}"
