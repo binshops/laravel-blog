b00d57cdb78c8dc1ea67c6fdc92a4c5e97080123
Sam <sam@binshops.com>
2021-12-14 13:58:28 -0600

    setup page

--- src/Views/binshopsblog_admin/setup/setup.blade.php
+++ src/Views/binshopsblog_admin/setup/setup.blade.php
@@ -5,7 +5,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Binshops Blog pacakge setup</title>
+    <title>Binshops Blog package setup</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
